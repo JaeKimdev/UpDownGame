be3370bc83fcecbe6b8fa51747023e69cfb468a2
Jae Hyung Kim <smfc.mene@gmail.com>
2022-09-01 22:36:01 +0800

    feat: Add `Credit` view

---
+++ UpDownGame/CreditViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by smfc on 1/9/2022.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+    
+    @IBAction func touchUpDismissButton(_ sender: UIButton) {
+        dismiss(animated: true,
+        completion: nil)
+    }
+}
