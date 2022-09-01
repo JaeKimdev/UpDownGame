ef6f1bc190d94298b3a530d4e2e32ff11af2c365
Jae Hyung Kim <smfc.mene@gmail.com>
2022-09-01 16:33:41 +0800

    feat: make functions for print sliderValue

---
+++ UpDownGame/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by smfc on 1/9/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    
+    @IBAction func touchUpHitButton(_ sender: UIButton) {
+        print(slider.value)
+    }
+
+}
+
